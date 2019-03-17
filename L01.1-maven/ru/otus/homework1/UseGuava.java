307fce8b1a7e104f2fc04787e7aebc421f0f4710
jan-kamilchu <47940745+jan-kamilchu@users.noreply.github.com>
2019-03-17 23:23:57 +0300

    Delete UseGuava.java

--- L01.1-maven/ru/otus/homework1/UseGuava.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.homework1;
-
-public class UseGuava {
-    public static void main(String... args){
-        SomeStuff ss = new SomeStuff("");
-        System.out.println(ss.DoSomeStuff());
-    }
-}
